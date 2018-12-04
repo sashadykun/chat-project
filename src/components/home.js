b0cd1a129a388b686a7b8360c70a8e7a6d57f1da
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-12-04 13:37:22 -0800

    have 404 page up

---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export default props => (
+    <div>
+         <h1 className="center">Chat</h1>
+         <p className="grey-text">The chatiest app around</p>
+    </div>
+)
