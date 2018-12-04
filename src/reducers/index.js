4ecd31809d87e69ae453e23507bf51fcd6edb71a
Oleksandr Dykun <sashaluckyman@gmail.com>
2018-12-04 11:33:21 -0800

    redux and initial routing setup complete

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import {reducer as form} from 'redux-form';
+import chat from './chat_reducer';
+
+
+const rootReducer = combineReducers({chat,form});
+
+export default rootReducer;
